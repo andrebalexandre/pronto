c4d923a43329471d6a2335eb9759c61045e00fcc
andrebalexandre <andre_alexandre@estudante.sesisenai.org.br>
2023-06-26 09:51:50 -0300

    alter andré

--- entrega.php
+++ entrega.php
@@ -1,13 +1,20 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="website icon " type="png"
+  href="img/logo.png">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pedido</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="entrega.css">
     <link rel="stylesheet" href="style.css">
     <!-- -->
+    
 </head>
 <body>
     <div class="principal">
@@ -18,10 +25,10 @@
                 <span id="hamburger"></span>
               </button>
               <ul id="menu" role="menu">
-                <li><a href="/">Minha Conta</a></li>
+                <li><a href="minhaconta.php">Minha Conta</a></li>
                 <li><a href="principal.html">Produtos</a></li>
                 <li><a href="sobrenos.html">Sobre nós</a></li>
-                <li><a href="carrinho.html">Carrinho</a></li>
+                <li><a href="carrinho.php">Carrinho</a></li>
                 <li><a href="fale_conosco.html">Fale conosco</a></li>
               </ul>
             </nav>
@@ -54,11 +61,43 @@
                     <div class="back"></div>
                     <div class="front"></div>
                     <div class="box"></div>
-                </div>
-               
+                </div>   
             </button>
-    
-        </div>
+    </div>
+<div class="avaliacao">
+    <h1>Avalie</h1>
+		<?php
+		if(isset($_SESSION['msg'])){
+			echo $_SESSION['msg']."<br><br>";
+			unset($_SESSION['msg']);
+		}
+		?>
+		<form method="POST" action="processa.php" enctype="multipart/form-data">
+
+			<div class="estrelas">
+				<input type="radio" id="vazio" name="estrela" value="" checked>
+				
+				<label for="estrela_um"><i class="fa"></i></label>
+				<input type="radio" id="estrela_um" name="estrela" value="1">
+				
+				<label for="estrela_dois"><i class="fa"></i></label>
+				<input type="radio" id="estrela_dois" name="estrela" value="2">
+				
+				<label for="estrela_tres"><i class="fa"></i></label>
+				<input type="radio" id="estrela_tres" name="estrela" value="3">
+				
+				<label for="estrela_quatro"><i class="fa"></i></label>
+				<input type="radio" id="estrela_quatro" name="estrela" value="4">
+				
+				<label for="estrela_cinco"><i class="fa"></i></label>
+				<input type="radio" id="estrela_cinco" name="estrela" value="5"><br><br>
+				
+				<input class="btavaliar" type="submit" value="Enviar">
+				
+			</div>
+  </div>
+		</form>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/gsap@3.11/dist/gsap.min.js"></script>
+
 </html>
